bda85396d54d2076b3ab22208e3979d75e8c042d
Anish Nath <anish2good@yahoo.co.in>
2021-10-26 19:55:48 +0530

    Create variables.tf

---
+++ lambda/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+}
+
+variable "function_name" {
+  description = "The name of the function to provision"
+}
